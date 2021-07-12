52c4ea5b5ba8f65a97292387a17bcb3e9a655681
Benjamin <benmneb@gmail.com>
2021-07-12 14:35:23 +0800

    improve icon descriptions

--- src/assets/mainProjects.js
+++ src/assets/mainProjects.js
@@ -9,10 +9,10 @@ export const mainProjects = [
 		image: 'https://i.ibb.co/6gBsR7d/Screen-Shot-2020-05-12-at-8-55-25-am.png',
 		subTitle: 'The worlds first complete vegan product guide to Australia.',
 		stack: [
-			{ icon: icons.ReactJS, description: 'Front-end made in ReactJS 17+' },
+			{ icon: icons.ReactJS, description: 'ReactJS, React-Router, Material-UI' },
 			{ icon: icons.Redux, description: 'State managed with Redux' },
-			{ icon: icons.NodeJS, description: 'NodeJS server' },
-			{ icon: icons.PostgreSQL, description: 'Data-based with PostgreSQL' },
+			{ icon: icons.NodeJS, description: 'NodeJS / ExpressJS server' },
+			{ icon: icons.PostgreSQL, description: 'PostgreSQL with Knex.js' },
 			{ icon: icons.AWS, description: 'Hosted on AWS Amplify' },
 			{ icon: icons.PWA, description: 'Installable as a PWA' },
 		],
@@ -25,13 +25,10 @@ export const mainProjects = [
 		image: 'https://i.ibb.co/m6sYBw2/Screen-Shot-2020-05-12-at-8-53-59-am.png',
 		subTitle: 'A 1,500-year-old classic Buddhist manual is now a modern web-app.',
 		stack: [
-			{ icon: icons.ReactJS, description: 'ReactJS 17+' },
+			{ icon: icons.ReactJS, description: 'ReactJS, useContext replicating Redux' },
 			{ icon: icons.PWA, description: 'Installable as a PWA' },
 		],
-		keyPoints: [
-			'State managed by React Context replicating Redux',
-			'Working with large data objects',
-		],
+		keyPoints: ['Working with large data objects'],
 	},
 	{
 		id: 3,
@@ -40,11 +37,10 @@ export const mainProjects = [
 		github: 'https://github.com/benmneb/four-noble-truths',
 		image: 'https://i.ibb.co/9Nqx1Pn/Screen-Shot-2020-05-12-at-8-52-19-am.png',
 		subTitle: 'Deep elaboration of the heart of Buddhas teachings - in his own words.',
-		stack: [{ icon: icons.ReactJS, description: 'ReactJS 17+' }],
-		keyPoints: [
-			'State managed by React Context replicating Redux use',
-			'Working with large data objects',
+		stack: [
+			{ icon: icons.ReactJS, description: 'ReactJS, useContext replicating Redux' },
 		],
+		keyPoints: ['Working with large data objects'],
 	},
 	{
 		id: 4,
