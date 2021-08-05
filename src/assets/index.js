b7c6e27e5817b59ad8193bb383e4c5bfbeb17b41
Benjamin <benmneb@gmail.com>
2021-08-05 17:45:46 +0800

    simplify icons imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,17 +1,6 @@
 export { StyledTheme } from './StyledTheme';
 export { GlobalStyles } from './GlobalStyles';
 
-export { HTML } from './icons/HTML';
-export { CSS } from './icons/CSS';
-export { Javascript } from './icons/Javascript';
-export { ReactJS } from './icons/ReactJS';
-export { Redux } from './icons/Redux';
-export { NodeJS } from './icons/NodeJS';
-export { MongoDB } from './icons/MongoDB';
-export { GitHub } from './icons/GitHub';
-export { PostgreSQL } from './icons/PostgreSQL';
-export { AWS } from './icons/AWS';
-export { PWA } from './icons/PWA';
-export { Jquery } from './icons/Jquery';
+export * from './icons';
 
 export { mainProjects } from './mainProjects';
